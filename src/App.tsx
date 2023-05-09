5b49497ed6459591370ae83379ae5b6f69fb6378
hcampos <hcampospro@gmail.com>
2023-05-09 16:45:49 +0200

    ➕ Add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
 const App = () => {
-  return (
-    <>
-      <h1>Password Generator</h1>
-    </>
-  )
+  return <h1 className='text-3xl font-bold underline'>Password Generator</h1>
 }
 
 export default App
