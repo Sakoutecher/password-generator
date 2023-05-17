f1ef21ec67375c039987ce7ece3f6ee461bafab9
hcampos <hcampospro@gmail.com>
2023-05-17 11:17:51 +0200

    ♻️ PasswordGenerator component and apply the newt font

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+//Components
+import { PasswordGenerator } from '#/PasswordGenerator'
+
 const App = () => {
   return (
     <div
       style={{ backgroundImage: 'url(gggrain.svg)' }}
-      className='w-screen h-screen bg-no-repeat bg-cover bg-center flex justify-center items-center'
+      className='w-screen h-screen bg-no-repeat bg-cover bg-center flex justify-center items-center font-jetbrains'
     >
-      Password Generator
+      <PasswordGenerator />
     </div>
   )
 }
