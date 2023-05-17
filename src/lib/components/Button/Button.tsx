cb67ec210d38740ac7441a41176ae67dd1459855
hcampos <hcampospro@gmail.com>
2023-05-17 11:26:59 +0200

    ♻️ Add onclik property

--- src/lib/components/Button/Button.tsx
+++ src/lib/components/Button/Button.tsx
@@ -3,13 +3,15 @@ import { FC } from 'react'
 type ButtonProps = {
   text: string
   style?: object
+  onClick?: () => void
 }
 
-export const Button: FC<ButtonProps> = ({ text, style }) => {
+export const Button: FC<ButtonProps> = ({ text, style, onClick }) => {
   return (
     <button
       style={style}
       className='uppercase w-full bg-blue-300 py-2 rounded text-sm hover:bg-blue-400'
+      onClick={onClick}
     >
       {text}
     </button>
