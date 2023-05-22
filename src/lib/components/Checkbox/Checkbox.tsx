a4a74d237c2e1549c1e10d44dee14a1ef45bedf2
hcampos <hcampospro@gmail.com>
2023-05-22 11:39:16 +0200

    ♻️ Use the usecheckboxstore

--- src/lib/components/Checkbox/Checkbox.tsx
+++ src/lib/components/Checkbox/Checkbox.tsx
@@ -1,22 +1,53 @@
 //Librairies
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
+
+//Hooks
+import { useCheckboxStore } from '../../hooks/useCheckbox'
 
 type CheckboxProps = {
   label: string
 }
 
 export const Checkbox: FC<CheckboxProps> = ({ label }) => {
+  const checkboxs = useCheckboxStore((state) => state.checkboxs)
+  const setCheckboxs = useCheckboxStore((state) => state.setCheckboxs)
+  const removeCheckboxs = useCheckboxStore((state) => state.removeCheckboxs)
+  const [isChecked, setIsChecked] = useState<boolean>(false)
+
+  const checkValueInArray = (value: string, array: string[]): boolean => {
+    return array.includes(value)
+  }
+
+  const handleClick = () => {
+    if (isChecked) {
+      removeCheckboxs(label)
+    } else {
+      setCheckboxs(label.toUpperCase())
+    }
+  }
+
+  useEffect(() => {
+    setIsChecked(checkValueInArray(label.toUpperCase(), checkboxs))
+  }, [checkboxs])
+
+  useEffect(() => {
+    setIsChecked(checkValueInArray(label.toUpperCase(), checkboxs))
+  })
+
   return (
     <div className='w-full bg-gray-300 rounded-md py-2 px-3 flex items-center'>
       <div className='flex items-center mr-2'>
         <input
-          id='default-checkbox'
+          id={label}
           type='checkbox'
-          value=''
           className='w-4 h-4 rounded'
+          checked={isChecked ? true : false}
+          onChange={handleClick}
         />
       </div>
-      <span className='uppercase text-sm'>{label}</span>
+      <label htmlFor={label} className='uppercase text-sm'>
+        {label}
+      </label>
     </div>
   )
 }
