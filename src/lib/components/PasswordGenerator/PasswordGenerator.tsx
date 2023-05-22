c3c833ec585b9597c68169eebcbaf7ea501bd1d3
hcampos <hcampospro@gmail.com>
2023-05-22 13:39:26 +0200

    ✨ Generate password

--- src/lib/components/PasswordGenerator/PasswordGenerator.tsx
+++ src/lib/components/PasswordGenerator/PasswordGenerator.tsx
@@ -1,19 +1,73 @@
 //Librairies
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 //Components
 import { Checkbox } from '#/Checkbox'
 import { Slider } from '#/Slider'
 import { Button } from '#/Button'
 
+//Utils
+import { generate } from '../../utils/generate'
+import { verifyUppercase } from '../../utils/verify'
+import { verifyLowercase } from '../../utils/verify'
+import { verifyNumbers } from '../../utils/verify'
+import { verifySymbols } from '../../utils/verify'
+
+//Hooks
+import { useSliderStore } from '../../hooks/useSlider'
+import { useCheckboxStore } from '../../hooks/useCheckbox'
+
 export const PasswordGenerator = () => {
+  const [generatedPassword, setGeneratedPassword] = useState<string>('')
+  const lenght = useSliderStore((state) => state.value)
+  const checkboxs = useCheckboxStore((state) => state.checkboxs)
+
   const copyClipboard = () => {
     navigator.clipboard.writeText(generatedPassword)
   }
 
+  const generatePassword = () => {
+    setGeneratedPassword(
+      generate(
+        lenght,
+        verifyLowercase(checkboxs),
+        verifyUppercase(checkboxs),
+        verifyNumbers(checkboxs),
+        verifySymbols(checkboxs)
+      )
+    )
+  }
+
+  useEffect(() => {
+    setGeneratedPassword(
+      generate(
+        lenght,
+        verifyLowercase(checkboxs),
+        verifyUppercase(checkboxs),
+        verifyNumbers(checkboxs),
+        verifySymbols(checkboxs)
+      )
+    )
+    console.log(checkboxs)
+  }, [])
+
+  useEffect(() => {
+    setGeneratedPassword(
+      generate(
+        lenght,
+        verifyLowercase(checkboxs),
+        verifyUppercase(checkboxs),
+        verifyNumbers(checkboxs),
+        verifySymbols(checkboxs)
+      )
+    )
+  }, [checkboxs, lenght])
+
   return (
     <div className='rounded-md w-6/12 bg-slate-200/20 backdrop-blur-sm p-5'>
-      <div className='w-full bg-gray-300 rounded-md px-3 py-4 mb-4 text-sm h-12'></div>
+      <div className='w-full bg-gray-300 rounded-md px-3 py-4 mb-4 text-sm h-12'>
+        {generatedPassword}
+      </div>
       <div className='flex w-full items-start justify-between gap-6 mb-4'>
         <div className='w-2/4 grid grid-cols-2 gap-3'>
           <Checkbox label={`Uppercase`} />
@@ -31,7 +85,7 @@ export const PasswordGenerator = () => {
         text={`Copy the password`}
         style={{ marginBottom: '1rem' }}
       />
-      <Button text={`Regenerate password`} />
+      <Button onClick={generatePassword} text={`Regenerate password`} />
     </div>
   )
 }
