1d4ae8b37281f16480d6c347a92caf59b5611e83
hcampos <hcampospro@gmail.com>
2023-05-22 14:36:50 +0200

    ♻️ Use toaster component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 //Components
 import { PasswordGenerator } from '#/PasswordGenerator'
+import { Toaster } from '#/Toaster'
 
 const App = () => {
   return (
     <div
       style={{ backgroundImage: 'url(gggrain.svg)' }}
-      className='w-screen h-screen bg-no-repeat bg-cover bg-center flex justify-center items-center font-jetbrains'
+      className='w-screen h-screen bg-no-repeat bg-cover bg-center flex justify-center items-center font-jetbrains relative'
     >
       <PasswordGenerator />
+      <Toaster />
     </div>
   )
 }
