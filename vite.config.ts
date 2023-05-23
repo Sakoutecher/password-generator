a7f6851fa9c38d5eff7f3dc7ea7ad32028369248
hcampos <hcampospro@gmail.com>
2023-05-23 11:39:06 +0200

    🐛 Fix path

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '#': resolve(__dirname, './src/lib/components/'),
-      '~/': resolve(__dirname, './public/'),
+      '~': resolve(__dirname, './public/'),
     },
   },
 })
